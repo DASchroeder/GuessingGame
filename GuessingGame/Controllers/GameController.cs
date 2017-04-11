29c3587c7c91ea58274faa029e3274c240d46b6c
Daniel Schroeder <danielschroederjr@gmail.com>
2017-04-11 06:07:03 -0400

    Intro to ASP.NET MVC

---
+++ GuessingGame/Controllers/GameController.cs
@@ -0,0 +1,33 @@
+﻿using GuessingGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {                 
+            Session["Answer"] = new Random().Next(1, 10); //Random number generator, find a better one for a challenge
+
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["Answer"];  //(int) is a cast, to tell the compiler it is an int 
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public ActionResult Index(GameModel model) //OVERLOAD
+        {
+            ViewBag.Win = GuessWasCorrect(model.Guess);
+
+            return View(model);
+        }
+    }
+}
